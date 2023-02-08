c53487fd04dd929de25d0e7ce33c58c0b335c445
Marlla Rolemberg <marllasousar@gmail.com>
2023-02-08 15:40:56 -0300

    componentes dinamic

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./small-card.component.scss']
 })
 export class SmallCardComponent {
-
+@Input()
+photoCover: string="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKzH_2sTD7fe2XL_NgksL0Dz-7oicdFmMB8w&usqp=CAU ";
+@Input()
+contentTitle: string="Tecnologia";
 }
