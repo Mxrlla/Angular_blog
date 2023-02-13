c6ae1aa843a31e5b0acce06e3192d5f8cf24a489
Marlla Rolemberg <marllasousar@gmail.com>
2023-02-13 11:54:05 -0300

    update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { ComponentsComponent } from './pages/components/components.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent
+
+  },
+  {
+    path: 'components',
+    component:ComponentsComponent
+
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
@@ -8,6 +9,7 @@ import { SmallCardComponent } from './components/small-card/small-card.component
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
+import { ComponentsComponent } from './pages/components/components.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,13 @@ import { HomeComponent } from './pages/home/home.component';
     MenuTitleComponent,
     SmallCardComponent,
     BigCardComponent,
-    HomeComponent
+    HomeComponent,
+    ComponentsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
